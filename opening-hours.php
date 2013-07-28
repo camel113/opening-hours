8017bae4103a60d55ee6fb0093df6758310f6ef8
Adrien Bigler <adrienbigler@MacBook-Pro-de-adrienbigler.local>
2013-07-28 15:35:31 +0200

    Changing days display in french (not in admin)

--- opening-hours.php
+++ opening-hours.php
@@ -34,49 +34,49 @@ function my_shortcode_function($atts,$content) {
     return    '
               <table id="abg-schedule">
               <tr>
-                <th>Monday</th>
+                <th>Lu.</th>
                 <td>'.$opening_hours[0].'</td>
                 <td>'.$opening_hours[1].'</td>
                 <td>'.$opening_hours[2].'</td>
                 <td>'.$opening_hours[3].'</td>
               </tr>
               <tr>
-                <th>Tuesday</th>
+                <th>Ma.</th>
                 <td>'.$opening_hours[4].'</td>
                 <td>'.$opening_hours[5].'</td>
                 <td>'.$opening_hours[6].'</td>
                 <td>'.$opening_hours[7].'</td>
               </tr>
               <tr>
-                <th>Wednesday</th>
+                <th>Me.</th>
                 <td>'.$opening_hours[8].'</td>
                 <td>'.$opening_hours[9].'</td>
                 <td>'.$opening_hours[10].'</td>
                 <td>'.$opening_hours[11].'</td>
               </tr>
               <tr>
-                <th>Thursday</th>
+                <th>Je.</th>
                 <td>'.$opening_hours[12].'</td>
                 <td>'.$opening_hours[13].'</td>
                 <td>'.$opening_hours[14].'</td>
                 <td>'.$opening_hours[15].'</td>
               </tr>
               <tr>
-                <th>Friday</th>
+                <th>Ve.</th>
                 <td>'.$opening_hours[16].'</td>
                 <td>'.$opening_hours[17].'</td>
                 <td>'.$opening_hours[18].'</td>
                 <td>'.$opening_hours[19].'</td>
               </tr>
               <tr>
-              <th>Saturday</th>
+              <th>Sa.</th>
                 <td>'.$opening_hours[20].'</td>
                 <td>'.$opening_hours[21].'</td>
                 <td>'.$opening_hours[22].'</td>
                 <td>'.$opening_hours[23].'</td>
               </tr>
               <tr>
-                <th>Sunday</th>
+                <th>Di.</th>
                 <td>'.$opening_hours[24].'</td>
                 <td>'.$opening_hours[25].'</td>
                 <td>'.$opening_hours[26].'</td>
@@ -87,37 +87,37 @@ function my_shortcode_function($atts,$content) {
           return    '
               <table id="abg-schedule">
               <tr>
-                <th>Monday</th>
+                <th>Lu.</th>
                 <td>'.$opening_hours[0].'</td>
                 <td>'.$opening_hours[1].'</td>
               </tr>
               <tr>
-                <th>Tuesday</th>
+                <th>Ma.</th>
                 <td>'.$opening_hours[4].'</td>
                 <td>'.$opening_hours[5].'</td>
               </tr>
               <tr>
-                <th>Wednesday</th>
+                <th>Me.</th>
                 <td>'.$opening_hours[8].'</td>
                 <td>'.$opening_hours[9].'</td>
               </tr>
               <tr>
-                <th>Thursday</th>
+                <th>Je.</th>
                 <td>'.$opening_hours[12].'</td>
                 <td>'.$opening_hours[13].'</td>
               </tr>
               <tr>
-                <th>Friday</th>
+                <th>Ve.</th>
                 <td>'.$opening_hours[16].'</td>
                 <td>'.$opening_hours[17].'</td>
               </tr>
               <tr>
-              <th>Saturday</th>
+              <th>Sa.</th>
                 <td>'.$opening_hours[20].'</td>
                 <td>'.$opening_hours[21].'</td>
               </tr>
               <tr>
-                <th>Sunday</th>
+                <th>Di.</th>
                 <td>'.$opening_hours[24].'</td>
                 <td>'.$opening_hours[25].'</td>
               </tr>
